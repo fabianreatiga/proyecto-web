3f0617765e9282d1f67fa893fb53fddb3ca80fe0
fabianreatiga <fabianreatiga@correo.com>
2025-12-12 11:37:52 -0500

    test

--- server.js
+++ server.js
@@ -18,10 +18,12 @@ console.log('MONGO_URI:', process.env.MONGO_URI);
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 })
+
   .then(() => console.log("✅ Conectado a MongoDB"))
   .catch(err => console.error("❌ Error al conectar a MongoDB:", err));
+
 
 // =======================
 // MODELO
