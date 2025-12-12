8d6df845afbcdb39a9d770116b237006decfeedc
fabianreatiga <fabianreatiga@correo.com>
2025-12-12 10:25:11 -0500

    test

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+console.log('MONGO_URI:', process.env.MONGO_URI);
+
+
 // Middleware
 app.use(cors());
 app.use(express.json());
